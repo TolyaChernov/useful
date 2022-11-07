727bde571c18c72c8b41f9f3f295ede65e70ff02
TolyaChernov <116725473+TolyaChernov@users.noreply.github.com>
2022-11-07 09:12:33 +0300

    Отредактировал формат

--- stepik/dec_to_bin.py
+++ stepik/dec_to_bin.py
@@ -1,12 +1,12 @@
 #!/bin/bash
-#!/usr/bin/python3
+# !/usr/bin/python3
 
 
 # dec to bin
-s=""
-t=int(input())
-n=1
-while t!=0:
-    s=str(t%2)+s
-    t=t//2
-print (s)
+s = ""
+t = int(input())
+n = 1
+while t != 0:
+    s = str(t % 2) + s
+    t = t // 2
+print(s)
--- stepik/glasnye-soglasnye.py
+++ stepik/glasnye-soglasnye.py
@@ -1,16 +1,16 @@
 #!/bin/bash
-#!/usr/bin/python3
+# !/usr/bin/python3
 
-## счетчик гласных и согласных
-g="ауоыиэяюёе"
-s="бвгджзйклмнпрстфхцчшщ"
-gl=0
-so=0
-words=input().lower()
-for i in range (len(words)):
+# счетчик гласных и согласных
+g = "ауоыиэяюёе"
+s = "бвгджзйклмнпрстфхцчшщ"
+gl = 0
+so = 0
+words = input().lower()
+for i in range(len(words)):
     if words[i] in g:
-        gl+=1
+        gl += 1
     elif words[i] in s:
-        so+=1
-print("Количество гласных букв равно ", gl, 
-"\n", "Количество согласных букв равно ", so, sep="")
+        so += 1
+print("Количество гласных букв равно ", gl,
+      "\n", "Количество согласных букв равно ", so, sep="")
