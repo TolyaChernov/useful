d8c6a5ac7cfe5099d521f51f6c8f349a7f303b72
TolyaChernov <116725473+TolyaChernov@users.noreply.github.com>
2022-11-09 23:55:34 +0300

    добавление нового

---
+++ stepik/kvadrat.py
@@ -0,0 +1,9 @@
+numbers = [0, 1, 2, 3, 4, 5, 6]
+print(*numbers)
+
+print(numbers[1], "         ", numbers[-2])
+print(numbers[2], "         ", numbers[-3])
+print(numbers[3], "         ", numbers[-4])
+print(numbers[4], "         ", numbers[-5])
+print(numbers[5], "         ", numbers[-6])
+print(*numbers[::-1])
